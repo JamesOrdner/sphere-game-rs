1b758b39689a3c9cfb6f0643a1fe1ff3c359fd4d
JamesOrdner <5782729+JamesOrdner@users.noreply.github.com>
2021-10-27 08:12:15 -0700

    Initial work on running simulation and render in parallel

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -38,8 +38,11 @@ impl Client {
         }
     }
 
-    pub fn run(mut self, event_loop: winit::event_loop::EventLoop<()>) -> ! {
-        println!("{}", std::mem::size_of_val(&self.systems.simulate()));
+    pub fn run(mut self, event_loop: EventLoop<()>) -> ! {
+        println!(
+            "{}",
+            std::mem::size_of_val(&self.systems.simulate_and_render())
+        );
 
         self.load_level();
         self.last_update = std::time::Instant::now();
@@ -59,26 +62,12 @@ impl Client {
 
                 self.flush_input();
                 self.distribute_events();
-                self.simulate();
-                self.distribute_events();
-                self.render();
+                self.simulate_and_render();
             }
             event => {
                 self.systems.input.handle_input(event);
             }
         });
-    }
-
-    fn simulate(&mut self) {
-        const UPDATE_INTERVAL: std::time::Duration = std::time::Duration::from_micros(16_666);
-
-        let time_now = std::time::Instant::now();
-        while time_now.duration_since(self.last_update) > UPDATE_INTERVAL {
-            self.last_update += UPDATE_INTERVAL;
-            let mut task = self.systems.simulate();
-            let task = unsafe { Pin::new_unchecked(&mut task) };
-            self.task_executor.execute_blocking(task);
-        }
     }
 
     fn flush_input(&mut self) {
@@ -87,14 +76,32 @@ impl Client {
         self.task_executor.execute_blocking(task);
     }
 
-    fn render(&mut self) {
-        let mut task = self.systems.render();
-        let task = unsafe { Pin::new_unchecked(&mut task) };
-        self.task_executor.execute_blocking(task);
-    }
-
     fn distribute_events(&mut self) {
         self.event_manager.distribute(&mut self.systems);
+    }
+
+    // fn simulate(&mut self) {
+    //     const UPDATE_INTERVAL: std::time::Duration = std::time::Duration::from_micros(16_666);
+
+    //     let time_now = std::time::Instant::now();
+    //     while time_now.duration_since(self.last_update) > UPDATE_INTERVAL {
+    //         self.last_update += UPDATE_INTERVAL;
+    //         let mut task = self.systems.simulate();
+    //         let task = unsafe { Pin::new_unchecked(&mut task) };
+    //         self.task_executor.execute_blocking(task);
+    //     }
+    // }
+
+    // fn render(&mut self) {
+    //     let mut task = self.systems.render();
+    //     let task = unsafe { Pin::new_unchecked(&mut task) };
+    //     self.task_executor.execute_blocking(task);
+    // }
+
+    fn simulate_and_render(&mut self) {
+        let mut task = self.systems.simulate_and_render();
+        let task = unsafe { Pin::new_unchecked(&mut task) };
+        self.task_executor.execute_blocking(task);
     }
 
     fn load_level(&mut self) {
@@ -140,6 +147,26 @@ impl Systems {
         let static_mesh = self.gfx_static_mesh.render();
 
         static_mesh.await;
+    }
+
+    pub async fn simulate_and_render(&mut self) {
+        let simulate = async {
+            let camera = self.sim_camera.simulate();
+            let physics = self.sim_physics.simulate();
+
+            camera.await;
+            physics.await;
+        };
+
+        let render = async {
+            let static_mesh = self.gfx_static_mesh.render();
+
+            static_mesh.await;
+        };
+
+        // todo: join rather than await sequentially
+        simulate.await;
+        render.await;
     }
 }
 
